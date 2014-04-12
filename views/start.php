b7615734ff0a2eb24aa4eafe8ac7425741cabebc
eoigal <eoigal@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-12 10:40:56 +0000

    need to handle case where akismet account is in cancelled state when on start page
    git-svn-id: https://plugins.svn.wordpress.org/akismet/trunk@891805 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- views/start.php
+++ views/start.php
@@ -33,7 +33,7 @@
 		<input type="submit" name="submit" id="submit" class="button button-secondary" value="<?php esc_attr_e('Use this key', 'akismet');?>">
 	</form>
 </div>
-<?php elseif ( $akismet_user->status == 'missing' ) : //no akismet account, ask do they want to use jetpack wpcom account to create one, then redirect to plans page?>
+<?php elseif ( in_array( $akismet_user->status, array( 'missing', 'no-sub' ) ) ) : //no akismet account, ask do they want to use jetpack wpcom account to create one, then redirect to plans page?>
 <p><?php esc_html_e('Akismet eliminates the comment and trackback spam you get on your site. Register your email address below to get started.', 'akismet'); ?></p>
 <div class="activate-highlight activate-option">
 	<div class="option-description">
@@ -45,6 +45,38 @@
 		<input type="hidden" name="auto-connect" value="<?php echo $akismet_user->ID;?>"/>
 		<input type="hidden" name="redirect" value="plugin-signup"/>
 		<input type="submit" class="button button-primary" value="<?php esc_attr_e( 'Register Akismet' , 'akismet'); ?>"/>
+	</form>
+</div>
+<div class="activate-highlight secondary activate-option">
+	<div class="option-description">
+		<strong><?php esc_html_e('Create a new API key with a different email address', 'akismet'); ?></strong>
+		<p><?php esc_html_e('Use this option if you want to setup a new Akismet account.', 'akismet'); ?></p>
+	</div>
+	<?php Akismet::view( 'get', array( 'text' => __( 'Register a different email address' , 'akismet'), 'classes' => array( 'right', 'button', 'button-secondary' ) ) ); ?>
+</div>
+<div class="activate-highlight secondary activate-option">
+	<div class="option-description">
+		<strong><?php esc_html_e('Manually enter an API key', 'akismet'); ?></strong>
+		<p><?php esc_html_e('If you have another API key you want to use.', 'akismet'); ?></p>
+	</div>
+	<form action="<?php echo esc_url( Akismet_Admin::get_page_url() ); ?>" method="post" id="akismet-enter-api-key" class="right">
+		<input id="key" name="key" type="text" size="15" maxlength="12" value="" class="regular-text code">
+		<input type="hidden" name="action" value="enter-key">
+		<?php wp_nonce_field( Akismet_Admin::NONCE ) ?>
+		<input type="submit" name="submit" id="submit" class="button button-secondary" value="<?php esc_attr_e('Use this key', 'akismet');?>">
+	</form>
+</div>
+<?php elseif ( $akismet_user->status == 'cancelled' ) : ?>
+<p><?php esc_html_e('Akismet eliminates the comment and trackback spam you get on your site.', 'akismet'); ?></p>
+<div class="activate-highlight activate-option">
+	<div class="option-description" style="width:75%;">
+		<strong class="small-heading"><?php esc_html_e('Connected via Jetpack', 'akismet'); ?></strong>
+		<?php printf( esc_html__( 'Your subscription for %s is cancelled' , 'akismet'), $akismet_user->user_email ); ?>
+	</div>
+	<form name="akismet_activate" id="akismet_activate" action="https://akismet.com/get/" method="post" class="right">
+		<input type="hidden" name="passback_url" value="<?php echo esc_attr( Akismet_Admin::get_page_url() ); ?>"/>
+		<input type="hidden" name="redirect" value="upgrade"/>
+		<input type="submit" class="button button-primary" value="<?php esc_attr_e( 'Reactivate Akismet' , 'akismet'); ?>"/>
 	</form>
 </div>
 <div class="activate-highlight secondary activate-option">
