9e38ff81f4fff7e6cd511c23fe4b125f0abfb577
eoigal <eoigal@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-15 13:50:18 +0000

    fix link to comment spam folder
    git-svn-id: https://plugins.svn.wordpress.org/akismet/trunk@893628 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- views/config.php
+++ views/config.php
@@ -88,7 +88,7 @@
 													<p><label for="akismet_strictness_1"><input type="radio" name="akismet_strictness" id="akismet_strictness_1" value="1" <?php checked('1', get_option('akismet_strictness')); ?> /> <?php esc_html_e('Silently discard the worst and most pervasive spam so I never see it.', 'akismet'); ?></label></p>
 													<p><label for="akismet_strictness_0"><input type="radio" name="akismet_strictness" id="akismet_strictness_0" value="0" <?php checked('0', get_option('akismet_strictness')); ?> /> <?php esc_html_e('Always put spam in the Spam folder for review.', 'akismet'); ?></label></p>
 													</fieldset>
-													<span class="note"><strong><?php esc_html_e('Note:', 'akismet');?></strong> <?php printf( __( 'Spam in the <a href="%s">spam folder</a> older than 15 days is deleted automatically.' , 'akismet'), admin_url( 'edit-comments.php?type=spam' ) );?></span>
+													<span class="note"><strong><?php esc_html_e('Note:', 'akismet');?></strong> <?php printf( __( 'Spam in the <a href="%s">spam folder</a> older than 15 days is deleted automatically.' , 'akismet'), admin_url( 'edit-comments.php?comment_status=spam' ) );?></span>
 												</td>
 											</tr>
 										</tbody>
